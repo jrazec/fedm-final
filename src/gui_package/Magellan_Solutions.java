95abbe5adf1d6d39056887c92bfbf778bf7c922c
jrazec <johnrazeagno@gmail.com>
2025-03-13 19:54:50 +0800

    Create landing page

--- src/gui_package/Magellan_Solutions.java
+++ src/gui_package/Magellan_Solutions.java
@@ -5,11 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class Magellan_Solutions extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -19,7 +26,7 @@ public class Magellan_Solutions extends JFrame {
 			public void run() {
 				try {
 					Magellan_Solutions frame = new Magellan_Solutions();
-					frame.setVisible(true);
+					frame.setVisible(true);s
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -32,11 +39,30 @@ public class Magellan_Solutions extends JFrame {
 	 */
 	public Magellan_Solutions() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 805, 479);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Magellan's Solutions");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblNewLabel.setBounds(290, 49, 195, 35);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(341, 196, 130, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Enter username:");
+		lblNewLabel_1.setBounds(341, 182, 129, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Admin", "Manager", "Employee"}));
+		comboBox.setBounds(340, 250, 131, 27);
+		contentPane.add(comboBox);
 	}
-
 }
